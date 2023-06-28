de5a8d432e401077b3eaefa28b371c1c3b11dda3
Luigi160397 <luigi.difraia.97@gmail.com>
2023-06-28 15:04:58 +0200

    Creata registrazione utente

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,112 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [register, setRegister] = useState({
+    username: "",
+    email: "",
+    password: "",
+    nome: "",
+    cognome: ""
+  });
+
+  const navigate = useNavigate();
+
+  const sendRegister = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3001/auth/register`, {
+        method: "POST",
+        body: JSON.stringify(register),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      if (response.ok) {
+        setRegister({
+          username: "",
+          email: "",
+          password: "",
+          nome: "",
+          cognome: ""
+        });
+
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
   return (
-    <Container>
-      <h1 className="text-light">register</h1>
+    <Container className="text-light d-flex justify-content-center align-items-center">
+      <Form
+        style={{
+          border: "3px solid #3f51b5 !important",
+          backgroundColor: "black"
+        }}
+        className=" rounded p-5 mt-5"
+        onSubmit={sendRegister}
+      >
+        <Form.Group className="mb-3">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci il tuo username"
+            value={register.username}
+            onChange={e => setRegister({ ...register, username: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Inserisci la tua email"
+            value={register.email}
+            onChange={e => setRegister({ ...register, email: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Inserisci la tua password"
+            value={register.password}
+            onChange={e => setRegister({ ...register, password: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Nome</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci il tuo nome"
+            value={register.nome}
+            onChange={e => setRegister({ ...register, nome: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Cognome</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci il tuo cognome"
+            value={register.cognome}
+            onChange={e => setRegister({ ...register, cognome: e.target.value })}
+          />
+        </Form.Group>
+        <div className="d-flex justify-content-end">
+          <Button
+            className="border-0"
+            style={{
+              backgroundColor: "#3f51b5"
+            }}
+            type="submit"
+          >
+            Registrati
+          </Button>
+        </div>
+      </Form>
     </Container>
   );
 };
