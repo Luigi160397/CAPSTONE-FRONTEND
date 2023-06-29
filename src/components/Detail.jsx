49f6203a5a575077516166436080f5aae5e7a60b
Luigi160397 <luigi.difraia.97@gmail.com>
2023-06-29 18:39:31 +0200

    Aggiunta la possibilita di aggiungere e rimuovere preferiti

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { Badge, Col, Container, Row } from "react-bootstrap";
+import { Alert, Badge, Button, Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getDettagioAction } from "../redux/actions";
+import { getDettagioAction, getPreferitiAction } from "../redux/actions";
 import CommentArea from "./CommentArea";
+import { FaRegStar, FaStar } from "react-icons/fa";
 
 const Detail = () => {
   const params = useParams();
@@ -12,10 +13,51 @@ const Detail = () => {
 
   useEffect(() => {
     dispatch(getDettagioAction(url));
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.idFilm]);
 
   const film = useSelector(state => state.home.film);
+  const preferiti = useSelector(state => state.home.preferiti);
+
+  const isPreferito = preferiti.length > 0 && preferiti.find(favourite => favourite.id === film.id);
+
+  const filmPref = { idFilm: film.id };
+
+  const token = localStorage.getItem("token");
+  const addPreferito = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/users/me/preferiti`, {
+        method: "POST",
+        body: JSON.stringify(filmPref),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      });
+      if (response.ok) {
+        dispatch(getPreferitiAction());
+      }
+    } catch (error) {
+      Alert(error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/users/me/preferiti/${film.id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      if (response.ok) {
+        dispatch(getPreferitiAction());
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <Container fluid className="text-light mt-5">
@@ -34,7 +76,7 @@ const Detail = () => {
           <Row className="justify-content-center gy-5">
             <Col md={8} className="order-2 order-md-1">
               <Row className="justify-content-center gy-5">
-                <Col md={10} className="text-center border border-1 rounded-3 card-film-dettaglio">
+                <Col md={10} className="text-center border border-1 rounded-3 card-film-dettaglio position-relative">
                   <h2 className="mt-3 display-3">{film.nome}</h2>
                   <p>{film.annoUscita}</p>
                   <p>{film.categoria}</p>
@@ -43,6 +85,37 @@ const Detail = () => {
                     Ratings: <Badge className="me-1 badge-ratings">{film.voto}</Badge>
                   </p>
                   <p>{film.descrizione}</p>
+                  {isPreferito ? (
+                    <Button
+                      onClick={handleDelete}
+                      type="button"
+                      className="fs-2"
+                      style={{
+                        position: "absolute",
+                        top: "10px",
+                        right: "10px",
+                        backgroundColor: "transparent",
+                        border: "0"
+                      }}
+                    >
+                      <FaStar />
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={addPreferito}
+                      type="button"
+                      className="fs-2"
+                      style={{
+                        position: "absolute",
+                        top: "10px",
+                        right: "10px",
+                        backgroundColor: "transparent",
+                        border: "0"
+                      }}
+                    >
+                      <FaRegStar />
+                    </Button>
+                  )}
                 </Col>
                 <Col md={10}>
                   <CommentArea params={params.idFilm} />
